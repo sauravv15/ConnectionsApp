42823d53f8c63b01285b015f13efb00608bfd708
sauravv15 <saurav.v.15@gmail.com>
2023-08-10 19:29:37 -0400

    Initial Commit

---
+++ SavorApp/SavorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavorAppApp.swift
+//  SavorApp
+//
+//  Created by Saurav Vidyadhara on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SavorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
